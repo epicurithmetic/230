fd255bdadbbfa754a4adc7405d1224e72c68e457
epicurithmetic <rhsculling@gmail.com>
2022-04-13 16:18:04 +1200

    Trying to figure out binary addition.

--- code/tm.py
+++ code/tm.py
@@ -213,8 +213,8 @@ def tm_read_head(tm_position,tm_state,tm_print,tm_move,tm_updateState):
 
 # Start the computations...
 turingMachineInput = read_tm_input("tm-tape.txt")
-turingMachineDictionary = tm_compile_dictionary("tm-code-binarydecrement.txt")
-turingMachineOutput = open("tm-output.txt","x")
+turingMachineDictionary = tm_compile_dictionary("tm-code-binaryaddition.txt")
+turingMachineOutput = open("tm-output.txt","w")
 turingMachineOutput.close()
 
 # NOTE!!! This should be refactored into a function/functions.
