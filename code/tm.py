0fa62da4584db9e200a8d2f25cfe810f4445635b
epicurithmetic <rhsculling@gmail.com>
2022-04-14 18:38:48 +1200

    To do: Comment the binary addition code.

--- code/tm.py
+++ code/tm.py
@@ -217,7 +217,7 @@ def tm_read_head(tm_position,tm_state,tm_print,tm_move,tm_updateState):
 
 # Start the computations...
 turingMachineInput = read_tm_input("tm-tape.txt")
-turingMachineDictionary = tm_compile_dictionary("tm-code-unaryAddition.txt")
+turingMachineDictionary = tm_compile_dictionary("tm-code-binaryaddition.txt")
 turingMachineOutput = open("tm-output.txt","w")
 turingMachineOutput.close()
 
